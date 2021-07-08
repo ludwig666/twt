e0def699c25dcaa38d50564961de7a99172fa698
ludwig666 <63304521+ludwig666@users.noreply.github.com>
2021-07-09 01:10:47 +0900

    try to send DraftTweet

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
 use std::env;
+use egg_mode::tweet::DraftTweet;
 
-fn main() {
+#[tokio::main]
+async fn main(){
     let args: Vec<String> = env::args().collect();
     if args.len() > 1 {
-        let message = &args[1];
-        println!("{}", message);
-    }
+        // let message = &args[1].clone() ;
+        let con_token = egg_mode::KeyPair::new("consumer key", "consumer secret");
+        let access_token = egg_mode::KeyPair::new("access token key", "access token secret");
+        let token = egg_mode::Token::Access {
+            consumer: con_token,
+            access: access_token,
+        };
+        let _tweet = DraftTweet::new("ちくわ大明神").send(&token).await.unwrap();
+        // println!("{}", &message);
+    };
 }
